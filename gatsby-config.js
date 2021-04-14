b60359ebc64cb4181131fd1470886a5e446f49ee
UncannyValle <jvalle0926@gmail.com>
2021-04-13 20:26:51 -0400

    updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `atole-tech`,
+        short_name: `atole`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
